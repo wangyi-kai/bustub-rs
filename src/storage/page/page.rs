342c1815a52c6bd24554522643e404bab1daaba2
wangyikai <466940603@qq.com>
2024-12-17 20:16:23 +0800

    initial commit

--- src/storage/page/page.rs
+++ src/storage/page/page.rs
@@ -26,10 +26,13 @@ pub struct PageHeader {
     prev_page_id: u32,
     next_page_id: u32,
     free_space_offset: u32,
+    is_dirty: bool,
+    pin_count: u32,
 }
 
 impl PageHeader {
     const SIZE: usize = std::mem::size_of(Self);
+
     #[inline]
     pub fn to_u8(&mut self) -> &[u8] {
         let bytes = unsafe {
@@ -44,10 +47,6 @@ impl PageHeader {
         let header = unsafe {
             std::ptr::read(bytes.as_ptr() as *const Self)
         };
-        // let self = unsafe {
-        //     let header = bytes.as_ptr() as *const PageHeader;
-        //     *header
-        // };
         header
     }
 }
@@ -60,5 +59,32 @@ pub struct Page {
     header: PageHeader,
     slot: Vec<Slot>,
     data: Vec<u8>,
+}
+
+impl Page {
+    #[inline(always)]
+    pub fn get_data(&self) -> &[u8] {
+        self.data
+    }
+
+    #[inline(always)]
+    pub fn get_id(&self) -> Option<u32> {
+        Some(self.header.page_id)
+    }
+
+    #[inline(always)]
+    pub fn get_pin_count(&self) -> Option<u32> {
+        Some(self.header.pin_count)
+    }
+
+    #[inline]
+    pub fn is_dirty(&self) -> Option<bool> {
+        Some(self.header.is_dirty)
+    }
+
+    #[inline(always)]
+    pub fn reset_memory(&mut self) {
+        self.data = vec![0; PAGE_SIZE];
+    }
 }
 
